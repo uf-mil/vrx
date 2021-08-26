335e1876e93940870e2ba372f58a7405186cc94c
Alex <alex.perez@ufl.edu>
2021-08-26 18:14:41 -0500

    updated vrx code

--- usv_gazebo_plugins/src/usv_gazebo_wind_plugin.cc
+++ usv_gazebo_plugins/src/usv_gazebo_wind_plugin.cc
@@ -132,6 +132,22 @@ void UsvWindPlugin::Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
 
   gzmsg << "update rate  = " << this->updateRate << std::endl;
 
+  if (_sdf->HasElement("topic_wind_speed"))
+  {
+    this->topicWindSpeed =
+      _sdf->GetElement("topic_wind_speed")->Get<std::string>();
+  }
+
+  gzmsg << "topic wind speed  = " << this->topicWindSpeed << std::endl;
+
+  if (_sdf->HasElement("topic_wind_direction"))
+  {
+    this->topicWindDirection =
+      _sdf->GetElement("topic_wind_direction")->Get<std::string>();
+  }
+
+  gzmsg << "topic wind direction  = " << this->topicWindDirection << std::endl;
+
   // Setting the  seed for the random generator.
   unsigned int seed = std::random_device {}();
   if (_sdf->HasElement("random_seed") &&
@@ -262,11 +278,7 @@ void UsvWindPlugin::Update()
   this->previousTime = currentTime;
 
   double publishingBuffer = 1/this->updateRate;
-  if (this->updateRate >= 0)
-  {
-    publishingBuffer = 1/this->updateRate;
-  }
-  else
+  if (this->updateRate < 0)
   {
     publishingBuffer = -1;
   }
